125dd9ff2d3da8d8f2fb1b960a3f066cad2fd940
Marshyswallow <v3331587@gmail.com>
2026-01-31 01:41:53 +0530

    backend

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require('express')
+const app=express()
+
+app.get('/ci',function(req,res){
+  const p=parseFloat(req.query.p)
+  const r=parseFloat(req.query.r)
+  const t=parseFaloat(req.query.t)
+
+  if(isNaN(p) || isNaN(r) || isNaN(t)){
+    return res.status(400).json({
+      msg: "invalid inputs"
+    });
+  }
+
+  const ci=p * Math.pow((1 + r/ 100), t)
+
+  res.send(ci.toFixed(2))
+})
+
+app.listen(3000,function(){
+  console.log("server is running")
+})
